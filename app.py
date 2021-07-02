67de3bdb7ccd8d9119c7a782c4723f5f871bd138
Olha Levko <olha.levko@avenga.com>
2021-07-02 16:53:33 +0300

    rebase pp_json method

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
 import json
 
-
-# insert the copied code below this line
-
-
-
-
 def pp_json(json_thing, sort=False, indents=2):
     if type(json_thing) is str:
         print(json.dumps(json.loads(json_thing), sort_keys=sort, indent=indents))
     else:
         print(json.dumps(json_thing, sort_keys=sort, indent=indents))
     return None
+
+# insert the copied code below this line
+
 
 pp_json(data.decode("utf-8"))
